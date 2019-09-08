4233a81ee30c42b1d72a91eebbbb3d8cf3d523f7
wangbiao <wangbiao@allinpay.com>
2019-09-08 21:50:41 +0800

    xinzheng deploy.sh

---
+++ WebContent/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+killTomcat(){
+   pid=`ps -ef|grep tomcat|grep java|awk `{print $2}
+   echo "tomcat id list :$pid"
+   if ["$pid"=""]
+   echo "tomcat pid value is null."
+   then
+	echo "no tomcat pid value."
+   else
+	kill -9 $pid
+   fi
+}
+cd $PROJ_PATH/TestWebProjcet
+mvn clean install
+
+killTomcat
+
+rm -rf $TOMCAT_APP_PATH/webapps/TestWebProjcet
+rm -rf $TOMCAT_APP_PATH/webapps/TestWebProjcet.war
+
+cp $PROJ_PATH/TestWebProjcet/target/TestWebProjcet.war $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
